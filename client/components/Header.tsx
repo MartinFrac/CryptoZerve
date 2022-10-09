eef391632445e83c3827965d54fe1b0b09f6d5db
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2022-10-09 17:19:29 +0100

    layout added

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-gray-300'>
-      Header
+    <div className='bg-gray-300 flex justify-between items-center px-8 py-2'>
+      <div className='flex flex-row gap-6 items-center'>
+        <span>CryptoZerve</span>
+        <div className='rounded-full bg-yellow-600 px-2 py-4 flex items-center'>USDT</div>
+      </div>
+      <div className='flex flex-row gap-6'>
+        <span>Vote</span>
+        <span>List your venue</span>
+        <span>Register</span>
+        <span>Login</span>
+      </div>
     </div>
   )
 }
