be447b25c826daf8fffa3ff32cfc5eafa9b76646
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2023-02-16 13:43:21 +0000

    added styles to header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -8,21 +8,23 @@ const Header = () => {
 
   return (
     <div className='bg-gray-300 flex justify-between items-center px-8 py-2'>
-      <div className='flex flex-row gap-6 items-center'>
-        <span>CryptoZerve</span>
+      <div className='flex flex-row gap-6 items-stretch'>
+        <Link href="/"><div className={styles}>CryptoZerve</div></Link>
         <div className='rounded-full bg-yellow-600 px-2 py-4 flex items-center'>USDT</div>
       </div>
       <div className='flex flex-row gap-6'>
-        <span>Vote</span>
-        <span>List your venue</span>
-        <span>Register</span>
+        <div className={styles}>Vote</div>
+        <div className={styles}>List your venue</div>
+        <div className={styles}>Register</div>
         {
           mmContext.account ? <span>{user}</span> : 
-          <button onClick={mmContext.connect}>Login</button>
+          <button onClick={mmContext.connect} className={styles}>Login</button>
         }
       </div>
     </div>
   )
 }
+
+const styles = 'cursor-pointer flex items-center px-4 py-4 hover:bg-slate-400 rounded-md';
 
 export default Header
