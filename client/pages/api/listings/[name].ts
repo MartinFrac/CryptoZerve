60961c75cb0d7cf5b66760e0d0460ffdec0f38e4
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2023-02-16 13:43:49 +0000

    changed api structure for listings

---
+++ client/pages/api/listings/[name].ts
@@ -0,0 +1,43 @@
+
+import { db } from "../../../config/firebase";
+import { collection, getDocs } from "firebase/firestore"; 
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type Data = {
+  id: string,
+  address: string;
+  owner: string;
+  name: string;
+  description: string;
+  price: Number;
+  venue: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+    const { query } = req;
+    const { name } = query;
+    console.log(`api/listings/${name}: executed`);
+    let listings: Data[] = [];
+    try {
+      const querySnapshot = await getDocs(collection(db, "booking_types"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        if (doc.data().name!=name) return;
+        listings.push({
+          id: doc.id,
+          address: doc.data().address,
+          owner: doc.data().owner,
+          name: doc.data().name,
+          description: doc.data().description,
+          price: doc.data().price,
+          venue: doc.data().venue,
+        })
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  res.status(200).json(listings);
+}
---
+++ client/pages/api/listings/index.ts
@@ -0,0 +1,40 @@
+import { db } from "../../../config/firebase";
+import { collection, getDocs } from "firebase/firestore"; 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type Data = {
+  id: string,
+  address: string;
+  owner: string;
+  name: string;
+  description: string;
+  price: Number;
+  venue: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+    console.log("api/listings: executed");
+    let listings: Data[] = [];
+    try {
+      const querySnapshot = await getDocs(collection(db, "booking_types"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        listings.push({
+          id: doc.id,
+          address: doc.data().address,
+          owner: doc.data().owner,
+          name: doc.data().name,
+          description: doc.data().description,
+          price: doc.data().price,
+          venue: doc.data().venue,
+        })
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  res.status(200).json(listings);
+}
