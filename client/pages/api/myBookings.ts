387cc539201d6748453b73d93689b3f3ab978900
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2023-02-19 22:10:52 +0000

    show my booking types wp

--- client/pages/api/myBookings.ts
+++ client/pages/api/myBookings.ts
@@ -1,7 +1,5 @@
-
 import { db } from "../../config/firebase";
 import { collection, getDocs } from "firebase/firestore"; 
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export type MyBookingData = {
--- client/pages/myBookingTypes.tsx
+++ client/pages/myBookingTypes.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useMMContext } from '../context/MetamaskContext';
+import MyBooking from '../components/MyBooking';
+import { Data } from './api/listings';
+import Listing from '../components/Listing';
 
 const myBookingTypes = () => {
+  const mmContext = useMMContext();
+  const user = mmContext.account;
+  const [myBookingTypesList, setMyBookingTypesList] = useState<Data[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/myBookings`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMyBookingTypesList(data);
+      })
+  }, []);
+
+  const myBookingsComponent = myBookingTypesList.map(item => {
+    return <Listing key={item.id} details={item} />
+  })
+
   return (
-    <div>myBookingTypes</div>
+    <div>
+      {myBookingsComponent}
+    </div>
   )
 }
 
