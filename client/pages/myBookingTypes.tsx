bcb359669a9089815a8488e43c60214d01390f1b
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2023-02-19 17:41:25 +0000

    created 2 additional pages: my bookings and my Booking types to be done

---
+++ client/pages/myBookingTypes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const myBookingTypes = () => {
+  return (
+    <div>myBookingTypes</div>
+  )
+}
+
+export default myBookingTypes
---
+++ client/pages/myBookings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const myBookings = () => {
+  return (
+    <div>myBookings</div>
+  )
+}
+
+export default myBookings
